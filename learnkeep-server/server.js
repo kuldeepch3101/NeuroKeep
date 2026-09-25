9780675fc7d91b2bf8dccca86abee16082335ce0
kuldeepch3101 <kuldeepch3101@gmail.com>
2026-09-25 16:36:51 +0530

    Update server.js

--- learnkeep-server/server.js
+++ learnkeep-server/server.js
@@ -34,8 +34,10 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log(err));
 
 const GEMINI_MODELS =[
+    "gemini-3.5-flash",
+    "gemini-3.5-flash-latest",
+    "gemini-3.5-flash-lite",
     "gemini-2.5-flash-lite",
-    "gemini-2.5-flash",
     "gemini-1.5-flash-latest",
     "gemini-1.5-flash",
     "gemini-pro"
